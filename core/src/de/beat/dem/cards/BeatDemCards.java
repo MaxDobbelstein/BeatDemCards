b660c35c0199c2fcf96434d82586a111bba83b57
Max Dobbelstein <max.dobbelstein@gmx.net>
2015-05-19 03:59:45 +0200

    showing something on the screen ... EXCITING!

--- core/src/de/beat/dem/cards/BeatDemCards.java
+++ core/src/de/beat/dem/cards/BeatDemCards.java
@@ -6,32 +6,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import de.beat.dem.cards.screens.MoveScreen;
 
 public class BeatDemCards extends Game {
-	SpriteBatch batch;
+	public SpriteBatch batch;
+	public BitmapFont bitmapFont;
+
 	Texture img;
 	private Rectangle bucket;
+
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		bucket = new Rectangle();
-		bucket.x = 0;
-		bucket.y = 0;
-		bucket.width = 64;
-		bucket.height = 64;
+		bitmapFont = new BitmapFont();
+		this.setScreen(new MoveScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, bucket.x, bucket.y);
-		batch.end();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+		super.render();
 	}
 }
--- core/src/de/beat/dem/cards/screens/MoveScreen.java
+++ core/src/de/beat/dem/cards/screens/MoveScreen.java
@@ -1,16 +1,45 @@
 package de.beat.dem.cards.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.Array;
 import de.beat.dem.cards.BeatDemCards;
+import de.beat.dem.cards.models.ActionStack;
+import de.beat.dem.cards.models.Card;
+import de.beat.dem.cards.models.Player;
 
 /**
  * Created by mkvr on 19.05.15.
  */
 public class MoveScreen implements Screen {
     private final BeatDemCards beatDemCards;
+    private OrthographicCamera camera;
+    private ActionStack actionStack;
+
 
     public MoveScreen(final BeatDemCards beatDemCards){
         this.beatDemCards = beatDemCards;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 640, 480);
+        Card cardOneKick = new Card("treten", 5, 1, 20);
+        Card cardOneHit = new Card("schlagen", 2, 5, 10);
+        Card cardTwoKick = new Card("treten", 5, 1, 20);
+        Card cardTwoHit = new Card("schlagen", 2, 5, 10);
+        Array<Card> cardOne = new Array<Card>();
+        cardOne.add(cardOneKick);
+        cardOne.add(cardOneHit);
+        Array<Card> cardTwo = new Array<Card>();
+        cardTwo.add(cardTwoKick);
+        cardTwo.add(cardTwoHit);
+
+        Player playerOne = new Player(cardOne, 100, 20, 0);
+        Player playerTwo = new Player(cardTwo, 100, 20, 1);
+
+        Array<Player> players = new Array<Player>();
+        players.add(playerOne);
+        players.add(playerTwo);
+
+        actionStack = new ActionStack(players);
     }
 
     @Override
@@ -20,6 +49,17 @@ public class MoveScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        camera.update();
+        beatDemCards.batch.setProjectionMatrix(camera.combined);
+
+        beatDemCards.batch.begin();
+        beatDemCards.bitmapFont.draw(beatDemCards.batch, "Player" + String.valueOf(actionStack.players.get(0).playerNo), 100, 100);
+        beatDemCards.bitmapFont.draw(beatDemCards.batch, "Hitpoints" + String.valueOf(actionStack.players.get(0).hitPoints), 100, 200);
+
+        beatDemCards.bitmapFont.draw(beatDemCards.batch, "Player" + String.valueOf(actionStack.players.get(1).playerNo), 400, 100);
+        beatDemCards.bitmapFont.draw(beatDemCards.batch, "Hitpoints" + String.valueOf(actionStack.players.get(1).hitPoints), 400, 200);
+
+        beatDemCards.batch.end();
 
     }
 
