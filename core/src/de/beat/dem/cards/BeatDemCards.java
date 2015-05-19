fc6afe10c33e831bf0747530146f192663506845
Max Dobbelstein <max.dobbelstein@gmx.net>
2015-05-19 02:07:48 +0200

    Initial Commit

---
+++ core/src/de/beat/dem/cards/BeatDemCards.java
@@ -0,0 +1,36 @@
+package de.beat.dem.cards;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public class BeatDemCards extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Rectangle bucket;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		bucket = new Rectangle();
+		bucket.x = 0;
+		bucket.y = 0;
+		bucket.width = 64;
+		bucket.height = 64;
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, bucket.x, bucket.y);
+		batch.end();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+	}
+}
---
+++ desktop/src/de/beat/dem/cards/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.beat.dem.cards.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.beat.dem.cards.BeatDemCards;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeatDemCards(), config);
+	}
+}
